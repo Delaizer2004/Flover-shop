36edd9a72b6e2086f9bc1e4f951aa56c7fe61647
Delaizer2004 <92630122+Delaizer2004@users.noreply.github.com>
2025-06-08 13:36:05 +0300

    Delete backend directory

--- backend/script.js
+++
@@ -1,8 +0,0 @@
-const bcrypt = require('bcrypt');
-
-(async () => {
-  const plain = 'admin';
-  const saltRounds = 10;
-  const hash = await bcrypt.hash(plain, saltRounds);
-  console.log(hash);
-})();
